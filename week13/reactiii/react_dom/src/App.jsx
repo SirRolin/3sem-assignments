dea1456a0ee8c58a249fcc23087a14e98a73d01b
Sir Rolin <nicolaieno@msn.com>
2024-05-09 21:04:17 +0200

    front end routing

--- week13/reactiii/react_dom/src/App.jsx
+++ week13/reactiii/react_dom/src/App.jsx
@@ -1,5 +1,14 @@
-import { useState } from 'react'
-import Header from './Components/Header'
+import { useState } from 'react';
+import { BrowserRouter as Router } from "react-router-dom";
+import Header from './Components/Header.jsx';
+import Entity from './Components/Entity.jsx';
+import EntityCreate from './Components/EntityCreate.jsx';
+import EntityDetails from './Components/EntityDetails.jsx';
+import Login from './Components/Login.jsx';
+import Register from './Components/Register.jsx';
+import Home from './Components/Home.jsx';
+import Settings from './Components/Settings.jsx';
+import Logout from './Components/Logout.jsx';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -7,6 +16,16 @@ function App() {
   return (
     <>
       <Header />
+      <Router>
+        <Route path="/Entity/" content={Entity}/>
+        <Route path="/EntityCreate/" content={EntityCreate}/>
+        <Route path="/EntityDetails/" content={EntityDetails}/>
+        <Route path="/Login/" content={Login}/>
+        <Route path="/Register/" content={Register}/>
+        <Route path="/Home/" content={Home}/>
+        <Route path="/Settings/" content={Settings}/>
+        <Route path="/Logout/" content={Logout}/>
+      </Router>
     </>
   )
 }
